36ba293246a776f8812d556ecbaca9f7e1860f7f
Darlei Mota <darleichans1@gmail.com>
2025-07-11 00:34:25 -0300

    Renomeando pastas, enviando solução exe_2

---
+++ src/main/java/com/darleimota/backendstage6/exercicios/exercicio1_1/ConversaoNumeros.java
@@ -0,0 +1,43 @@
+/*
+1 - Imagine que você tem uma lista de strings. Algumas das strings são números,
+mas outras não. Queremos converter a lista de strings para uma lista de números.
+Se a conversão falhar, você deve ignorar o valor.
+ */
+
+package com.darleimota.backendstage6.exercicios.exercicio1_1;
+
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+//@Component
+public class ConversaoNumeros implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<String> input = Arrays.asList("1", "abc", "20", "30x");
+
+        // Resposta feita atraves de pesquisas de melhor solução
+        List<Integer> numerosValidos = input.stream()
+                .filter(n -> n.matches("\\d+")) // Esse matches filtra os inteiros positivos
+                .map(Integer::parseInt) // Converte o que conseguiu de números em inteiros
+                .toList(); // Coleta para uma lista, não tiver a lista fica vazia.
+
+        // * Respotas aprendida em Aula, try, catch
+//        List<Integer> numerosValidos = input.stream()
+//                .map(str -> {
+//                    try {
+//                        return Optional.of(Integer.parseInt(str));
+//                    } catch (NumberFormatException e) {
+//                        return Optional.<Integer>empty(); }
+//                })
+//                .filter(Optional::isPresent)
+//                .map(Optional::get)
+//                .toList();
+
+        System.out.println("\n==== SEPARAÇÃO E CONVERSÃO EM NÚMEROS INTEIROS ====");
+        System.out.println("Lista a ser filtrada: " + input);
+        System.out.println("Nova lista separada: " + numerosValidos);
+    }
+}
---
+++ src/main/java/com/darleimota/backendstage6/exercicios/exercicio1_2/ProcessarNumero.java
@@ -0,0 +1,31 @@
+/*
+2 - Implemente um método que recebe um número inteiro dentro de um Optional.
+Se o número estiver presente e for positivo, calcule seu quadrado.
+Caso contrário, retorne Optional.empty.
+ */
+
+package com.darleimota.backendstage6.exercicios.exercicio1_2;
+
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Optional;
+
+//@Component
+public class ProcessarNumero implements CommandLineRunner {
+
+    public static Optional<Integer> processaNumero(Optional<Integer> numero) {
+
+        // Usando FlatMap porque estamos já trabalhando com Optional, e para fazer em uma linha só
+        // fazendo um if/else lambda pra comparar.
+        return numero.flatMap(n -> n > 0 ? Optional.of(n * n) :Optional.empty());
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("\nSe = numero então: numero * numero, senão: empty(Vazio)");
+        System.out.println(processaNumero(Optional.of(5))); // Saída: Optional[25]
+        System.out.println(processaNumero(Optional.of(-3))); // Saída: Optional.empty
+        System.out.println(processaNumero(Optional.empty())); // Saída: Optional.empty
+    }
+}
